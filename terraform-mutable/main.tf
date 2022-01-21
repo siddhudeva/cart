5f5ab0b13ddbfce81a1e181365e308a13e7644b3
siddhu <siddhudangeti@gmail.com>
2022-01-21 16:11:49 +0530

    url edited with DNS name

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -1,8 +1,8 @@
 module "app-module" {
-  source = "https://github.com/siddhudeva/terraform_mutable_commom_infra.git"
-  INSTANCES_NO = var.INSTANCES_NO
+  source         = "https://github.com/siddhudeva/terraform_mutable_commom_infra.git"
+  INSTANCES_NO   = var.INSTANCES_NO
   INSTANCES_TYPE = var.INSTANCES_TYPE
-  COMPONENT = var.COMPONENT
-  ENV = var.ENV
-  APP_VERSION = var.APP_VERSION
+  COMPONENT      = var.COMPONENT
+  ENV            = var.ENV
+  APP_VERSION    = var.APP_VERSION
 }
